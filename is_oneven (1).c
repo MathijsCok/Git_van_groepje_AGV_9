7371cb6bb3437bdaccce9f43120d60397baa8151
Bartjuh789 <137168354+Bartjuh789@users.noreply.github.com>
2023-06-22 10:52:58 +0200

    Add files via upload

---
+++ is_oneven (1).c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//hoi leks ik verander dit 
+
+int main()
+{
+  int getal = 1;
+
+while(1)
+{
+  printf("getal:\n");
+  scanf("%d", &getal);
+  printf("\n");
+
+  while(1)
+  {
+      if(getal == 0)
+        {
+          printf("no\n");
+          break;
+        }
+        else
+        {
+          if(getal == 1)
+          {
+            printf("yes\n");
+            break;
+          }
+          else
+          {
+            getal = getal - 2;
+          }
+        }
+  }
+  printf("\n\n");
+}
+  return(0);
+}
